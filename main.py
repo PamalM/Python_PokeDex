65315ecad38f9cbf7d092f3812606c22e5d54d8f
Pamal Mangat <mangatpamal@gmail.com>
2020-06-02 14:34:45 -0700

    - Api fetching implementation complete. - Successfuly retrieves and saves information for particular searched pokemon. - TODO: Create Graphical User Interface

--- main.py
+++ main.py
@@ -4,49 +4,63 @@ import requests
 import json
 import os
 from texttable import Texttable
+import tkinter as tk
+from PIL import Image, ImageTk
 
-# Function handles API Request and fetches information for the specified Pokemon.
-def api_Request(pokemon):
-    # Url to make API request. (Insert pokemon name, or id # at the end of url)
+
+# Function performs the API request for information for the specified pokemon.
+def fetch_JSON(pokemon):
+
+    # url for pokeAPI database.
     url = "https://pokeapi.co/api/v2/pokemon/" + pokemon.lower()
-    evolutions_Url = '/evolution-chain/{param1}/'
 
-    # Data directory will hold .json data locally for each searched pokemon.
-    data_Path = 'DATA/' + pokemon.upper()
+    # Perform API request and output the response code from the fetch.
+    page = requests.get(url)
+    response = page.status_code
+    print("\n[API Response: %s]" % page.status_code)
 
-    # Create .json file for specific pokemon if it doesn't already exists.
-    # No need to make API request if we have local Pokemon data available.
-    try:
-        # Create target Directory
-        os.mkdir(data_Path)
-        print('[{%s} DIRECTORY CREATED.]' % pokemon.upper())
+    # Check for a valid response from the API.
+    if response == 200:
 
-        # Make API request.
-        page = requests.get(url)
-        print("[API Response: %s]" % page.status_code)
+        # Data directory will hold (.json) data locally for each searched pokemon.
+        data_Path = 'DATA/' + pokemon.upper()
 
-        # Convert to Python .json object.
-        src = page.json()
+        try:
+            # Convert to Python (.json) object.
+            src = page.json()
 
-        # Write json to local file in DATA directory.
-        file = open(data_Path + "/" + pokemon.upper(), "w+")
-        file.write(json.dumps(src))
-        file.close()
+            # Create directory for pokemon's stats.
+            os.mkdir(data_Path)
+            print('[{%s} DIRECTORY CREATED.]' % pokemon.upper())
 
-    # Otherwise the directory already has information stored for this pokemon; No API request necessary.
-    except FileExistsError:
-        print('[{%s} DIRECTORY ALREADY EXISTS.]' % pokemon.upper())
+            # Write (json) to file to be saved.
+            file = open(data_Path + "/" + pokemon.upper(), "w+")
+            file.write(json.dumps(src))
+            file.close()
 
+        # Information for this pokemon already exists in a local directory.
+        except FileExistsError:
+            print('[{%s} DIRECTORY ALREADY EXISTS.]' % pokemon.upper())
+
+        return response
+
+    # Bad request made (error in pokemon name or id).
+    else:
+        return response
+
+
+# Read and return dictionary of stats for specified pokemon from json data file.
 def read_JSON(pokemon):
+
     # Open and load json file for reading.
-    file =  open(('DATA/' + pokemon.upper() + "/" + pokemon.upper()))
+    file = open(('DATA/' + pokemon.upper() + "/" + pokemon.upper()))
     data = json.load(file)
 
     # Extract Pokemon name and Pokedex Id from API data.
     id = data['id']
     name = data['name']
 
-    # Extract the type(s). Pokemon could either have 1 Type, or 2.
+    # Extract the type(s). Pokemon could either have 1-2 types.
     type1 = data['types'][0]['type']['name']
     type2 = data['types'][1]['type']['name'] if len(data['types']) == 2 else 0
     types = [type1, type2] if len(data['types']) == 2 else [type1]
@@ -77,18 +91,106 @@ def read_JSON(pokemon):
     table.set_cols_width([4, 15, 20, 4, 4, 4, 4, 4, 4, 6, 8, 6])
     print(table.draw())
 
-    # Put all statistics into one list to reduce amount of parameters.
-    statistics = [hp, attack, defence, spAttack, spDefence, speed]
+    # Format height and weights decimal points (limit to 2 decimal places).
+    weight_Format = "%.2f" % weight
+    height_Format = "%.2f" % height
 
-    values = {'id': id, 'name': name.upper(), 'attack': attack, 'defence': defence, 'spAttack': spAttack, 'spDefence': spDefence,
-              'speed': speed, 'height': height, 'weight': weight, 'totalStats': totalStats}
+    values = {'id': id, 'name': name.upper(), 'type': types, 'hp': hp, 'attack': attack, 'defence': defence, 'spAttack': spAttack,
+              'spDefence': spDefence, 'speed': speed, 'height': height_Format,
+              'weight': weight_Format, 'totalStats': totalStats, 'imageUrl': image}
 
     # Return dictionary containing all relevant information of interest from json.
     return values
 
 
-# api_Request('Charmander')
-data = read_JSON('charmander')
+# Function creates GUI for user to search pokemon by their name or id.
+def searchPokemon():
+
+    # Function re-sizes background image to be same size on window resize.
+    def resize_BG_master(event):
+        new_width = master.winfo_width()
+        new_height = master.winfo_height()
+        image = copy_of_image.resize((new_width, new_height))
+        photo = ImageTk.PhotoImage(image)
+        background.config(image=photo)
+        # Avoid garbage collection.
+        background.image = photo
+
+    # Function transfers user to next screen to view pokemon stats.
+    def submitSearch(pokemon):
+
+        # Get response code from API.
+        response = fetch_JSON(pokemon)
+
+        # If the response was valid from the API (True = 200) then proceed to next GUI.
+        if response == 200:
+            data = read_JSON(pokemon)
+            displayPokemon(data)
+
+        # This means that the response code was invalid (False = 404, etc). Display error screen.
+        else:
+            print(str(response) + " to be implemented")
+
+    # Create tkinter window object.
+    master = tk.Tk()
+
+    # Create label for GUI background image.
+    loadImage = Image.open("background.png")
+    copy_of_image = loadImage.copy()
+    background_image = ImageTk.PhotoImage(loadImage)
+    background = tk.Label(master, image=background_image)
+    background.pack()
+
+    label1 = tk.Label(background, text='ENTER POKÉMON BY ID OR NAME:', background='black')
+    label1.config(font=('Helvetica 30 bold'), fg='white')
+    label1.pack(fill=tk.BOTH, padx=80, pady=(120, 10))
+
+    # String variable to contain searched pokemon name/id from entry().
+    search = tk.StringVar()
+
+    searchBar = tk.Entry(background, textvariable=search, font="Helvetica 44 bold", justify="center")
+    searchBar.pack(fill=tk.BOTH, padx=80, pady=14)
+
+    searchButton = tk.Button(background, text='SEARCH', font='Helvetica 40 bold')
+    searchButton.config(highlightbackground='red', fg='white', command=lambda: submitSearch(search.get()))
+    searchButton.pack(fill=tk.BOTH, padx=225)
+
+    # Window attributes for tkinter object.
+    master.title('Python Pokédex')
+    master.geometry('750x500')
+    master.bind('<Configure>', resize_BG_master)
+    master.mainloop()
+
+
+def displayPokemon(data):
+
+    # Function re-sizes background image to be same size on window resize.
+    def resize_BG_root(event):
+        new_width = root.winfo_width()
+        new_height = root.winfo_height()
+        image = copy_of_Image_Display.resize((new_width, new_height))
+        photo = ImageTk.PhotoImage(image)
+        background_Display.config(image=photo)
+        # Avoid garbage collection.
+        background_Display.image = photo
+
+    root = tk.Toplevel()
+
+    # Create label for GUI background image.
+    loadImage_Display = Image.open("background_Display.png")
+    copy_of_Image_Display = loadImage_Display.copy()
+    background_Image_Display = ImageTk.PhotoImage(loadImage_Display)
+    background_Display = tk.Label(root, image=background_Image_Display)
+    background_Display.pack()
+
+    root.title('Python Pokédex')
+    root.geometry('750x500')
+    root.bind('<Configure>', resize_BG_root)
+    root.mainloop()
+
+
+print('[Python Pokédex booted up.]')
+searchPokemon()
 
 
 
